4395dd0495a0f9b853c86add709e9b690a148ddc
Alessandro Aglietti <alessandro-aglietti@users.noreply.github.com>
2016-08-02 20:32:22 +0200

    alerts & activation flow

--- js/main.js
+++ js/main.js
@@ -5,31 +5,34 @@ $(function(){
   Notify = window.Notify.default;
 
   if (!Notify.needsPermission) {
-    $("#enable").remove();
+    onPermissionGranted();
   }
 
   $("#enable").on('click', function(){
     if (!Notify.needsPermission) {
-      $("#enable").remove();
+      onPermissionGranted();
     } else if (Notify.isSupported()) {
         Notify.requestPermission(onPermissionGranted, onPermissionDenied);
+    } else {
+      alert("browser doesn't support notification api, try latest chrome!");
     }
   });
-
-  if ('serviceWorker' in navigator) {
-   console.log('Service Worker is supported');
-   navigator.serviceWorker.register('js/sw.js').then(function(reg) {
-     console.log(':^)', reg);
-     // TODO
-   }).catch(function(err) {
-     console.log(':^(', err);
-   });
-  }
 });
 
 function onPermissionGranted() {
     console.log('Permission has been granted by the user');
     $("#enable").remove();
+    if ('serviceWorker' in navigator) {
+     console.log('Service Worker is supported');
+     navigator.serviceWorker.register('js/sw.js').then(function(reg) {
+       console.log(':^)', reg);
+       // TODO
+     }).catch(function(err) {
+       console.log(':^(', err);
+     });
+    } else {
+      alert("browser doesn't support service workers, try latest chrome!");
+    }
 }
 
 function onPermissionDenied() {
