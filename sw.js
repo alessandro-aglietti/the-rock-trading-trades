1de46251ea175b4007cd4723b8397277e611143a
Your Name <alessandro.aglietti@gmail.com>
2016-08-02 22:53:42 +0200

    top

--- sw.js
+++ sw.js
@@ -1,22 +1,26 @@
 importScripts("js/vendor/pusher.min.js");
 
-function pusherr(){
-  var pusher = new Pusher('8458eb6fbd288f0cf3d8', {
-    encrypted: true
-  });
+var pusher;
 
-  var channel = pusher.subscribe('currency');
-  channel.bind('new_offer', function(data) {
-    console.log(data);
-    if ( data.symbol === "BTCEUR") {
-      var text = data.type + " " + data.value;
-      self.registration.showNotification(text, {
-        body: text,
-        icon: "apple-touch-icon.png",
-        tag: "new-trade"
-      });
-    }
-  });
+function pusherr(){
+  if( !pusher ) {
+    pusher = new Pusher('8458eb6fbd288f0cf3d8', {
+      encrypted: true
+    });
+
+    var channel = pusher.subscribe('currency');
+    channel.bind('new_offer', function(data) {
+      console.log(data);
+      if ( data.symbol === "BTCEUR") {
+        var text = data.type + " " + data.value;
+        self.registration.showNotification(text, {
+          body: text,
+          icon: "apple-touch-icon.png",
+          tag: "new-trade"
+        });
+      }
+    });
+  }
 }
 
 console.log('Started', self);
