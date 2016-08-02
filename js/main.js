129f262a4b30360cce8a3747696ce0e625e268ce
Your Name <alessandro.aglietti@gmail.com>
2016-08-02 20:25:49 +0200

    ma che bellezza

---
+++ js/main.js
@@ -0,0 +1,37 @@
+var Notify;
+
+$(function(){
+  console.log("onload");
+  Notify = window.Notify.default;
+
+  if (!Notify.needsPermission) {
+    $("#enable").remove();
+  }
+
+  $("#enable").on('click', function(){
+    if (!Notify.needsPermission) {
+      $("#enable").remove();
+    } else if (Notify.isSupported()) {
+        Notify.requestPermission(onPermissionGranted, onPermissionDenied);
+    }
+  });
+
+  if ('serviceWorker' in navigator) {
+   console.log('Service Worker is supported');
+   navigator.serviceWorker.register('/js/sw.js').then(function(reg) {
+     console.log(':^)', reg);
+     // TODO
+   }).catch(function(err) {
+     console.log(':^(', err);
+   });
+  }
+});
+
+function onPermissionGranted() {
+    console.log('Permission has been granted by the user');
+    $("#enable").remove();
+}
+
+function onPermissionDenied() {
+    console.warn('Permission has been denied by the user');
+}
---
+++ js/sw.js
@@ -0,0 +1,30 @@
+importScripts("vendor/pusher.min.js");
+
+console.log('Started', self);
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
+  console.log('Installed', event);
+});
+self.addEventListener('activate', function(event) {
+  console.log('Activated', event);
+  var pusher = new Pusher('8458eb6fbd288f0cf3d8', {
+      encrypted: true
+    });
+
+    var channel = pusher.subscribe('currency');
+    channel.bind('new_offer', function(data) {
+      console.log(data);
+      if ( data.symbol === "BTCEUR") {
+        var text = data.type + " " + data.value;
+        self.registration.showNotification(text, {
+          body: text,
+          icon: "../apple-touch-icon.png",
+          tag: "new-trade"
+        });
+      }
+    });
+});
+self.addEventListener('push', function(event) {
+  console.log('Push message received', event);
+  // TODO
+});
