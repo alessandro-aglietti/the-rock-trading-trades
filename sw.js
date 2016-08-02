cf47c5810be3a4fa1581c70b9732f976f1e48898
Your Name <alessandro.aglietti@gmail.com>
2016-08-02 22:25:27 +0200

    la follia delle path

--- sw.js
+++ sw.js
@@ -7,22 +7,6 @@ self.addEventListener('install', function(event) {
 });
 self.addEventListener('activate', function(event) {
   console.log('Activated', event);
-  var pusher = new Pusher('8458eb6fbd288f0cf3d8', {
-      encrypted: true
-    });
-
-    var channel = pusher.subscribe('currency');
-    channel.bind('new_offer', function(data) {
-      console.log(data);
-      if ( data.symbol === "BTCEUR") {
-        var text = data.type + " " + data.value;
-        self.registration.showNotification(text, {
-          body: text,
-          icon: "apple-touch-icon.png",
-          tag: "new-trade"
-        });
-      }
-    });
 });
 self.addEventListener('fetch', function(event) {
   console.log(event.request.url);
@@ -34,3 +18,24 @@ self.addEventListener('fetch', function(event) {
     );
   }
 });
+
+function pusherr(){
+var pusher = new Pusher('8458eb6fbd288f0cf3d8', {
+    encrypted: true
+  });
+
+  var channel = pusher.subscribe('currency');
+  channel.bind('new_offer', function(data) {
+    console.log(data);
+    if ( data.symbol === "BTCEUR") {
+      var text = data.type + " " + data.value;
+      self.registration.showNotification(text, {
+        body: text,
+        icon: "apple-touch-icon.png",
+        tag: "new-trade"
+      });
+    }
+  });
+}
+
+pusher();
