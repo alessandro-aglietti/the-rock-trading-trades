7a9381b094170321bb21aa95aefe235ca2a6e664
Your Name <alessandro.aglietti@gmail.com>
2016-08-02 20:58:34 +0200

    no more libraries

--- js/main.js
+++ js/main.js
@@ -2,29 +2,37 @@ var Notify;
 
 $(function(){
   console.log("onload");
-  Notify = window.Notify.default;
 
-  if (!Notify.needsPermission) {
+  if (!("Notification" in window)) {
+    alert("This browser does not support desktop notification");
+    return;
+  } else if (Notification.permission === "granted") {
+    // If it's okay let's create a notification
     $("#enable").remove();
   }
 
   $("#enable").on('click', function(){
-    if (!Notify.needsPermission) {
-      $("#enable").remove();
-    } else if (Notify.isSupported()) {
-        Notify.requestPermission(onPermissionGranted, onPermissionDenied);
-    }
-  });
-
-  if ('serviceWorker' in navigator) {
-   console.log('Service Worker is supported');
-   navigator.serviceWorker.register('/js/sw.js').then(function(reg) {
-     console.log(':^)', reg);
-     // TODO
-   }).catch(function(err) {
-     console.log(':^(', err);
-   });
+    if (Notification.permission !== 'denied') {
+      Notification.requestPermission(function (permission) {
+        // If the user accepts, let's create a notification
+        if (permission === "granted") {
+          if ('serviceWorker' in navigator) {
+           console.log('Service Worker is supported');
+           navigator.serviceWorker.register('/js/sw.js').then(function(reg) {
+             console.log(':^)', reg);
+             // TODO
+           }).catch(function(err) {
+             console.log(':^(', err);
+           });
+          }
+        }
+      });
+  } else {
+    alert("hai bloccato le notifiche scoglio!");
   }
+
+
+
 });
 
 function onPermissionGranted() {
