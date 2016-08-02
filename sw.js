277b99ac5308027fc6dc5e938421c79b7cb3c80d
Your Name <alessandro.aglietti@gmail.com>
2016-08-02 22:14:31 +0200

    did you know scope motafakerrrr

---
+++ sw.js
@@ -0,0 +1,36 @@
+importScripts("vendor/pusher.min.js");
+
+console.log('Started', self);
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
+  console.log('Installed', event);
+});
+self.addEventListener('activate', function(event) {
+  console.log('Activated', event);
+  var pusher = new Pusher('8458eb6fbd288f0cf3d8', {
+      encrypted: true
+    });
+
+    var channel = pusher.subscribe('currency');
+    channel.bind('new_offer', function(data) {
+      console.log(data);
+      if ( data.symbol === "BTCEUR") {
+        var text = data.type + " " + data.value;
+        self.registration.showNotification(text, {
+          body: text,
+          icon: "../apple-touch-icon.png",
+          tag: "new-trade"
+        });
+      }
+    });
+});
+self.addEventListener('fetch', function(event) {
+  console.log(event.request.url);
+  if (/\.sw$/.test(event.request.url)) {
+    event.respondWith(
+      new Response('magic goes here', {
+       headers: { 'Content-Type': 'text/plain' }
+     })
+    );
+  }
+});
