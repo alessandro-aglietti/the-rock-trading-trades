81995763fc802037e472df9e9f468f392384318e
Your Name <alessandro.aglietti@gmail.com>
2016-09-02 10:49:43 +0200

    fix avg math
    added colored icons based on spread
    
    	new file:   trt.blue.png
    	renamed:    trt.png -> trt.green.png
    	new file:   trt.orange.png
    	new file:   trt.red.png
    	new file:   trt.yellow.png
    	new file:   trt32.blue.png
    	renamed:    trt32.png -> trt32.green.png
    	new file:   trt32.orange.png
    	new file:   trt32.red.png
    	new file:   trt32.yellow.png

--- pusher-node/index.js
+++ pusher-node/index.js
@@ -4,10 +4,11 @@ var exec = require('child_process').exec;
 var pusher = new Pusher("8458eb6fbd288f0cf3d8");
 
 var last_ten = [];
+var last_ten_avg = 0;
 
-function last_ten_avg(now) {
+function do_last_ten_avg(now) {
   if ( last_ten.length > 10 ) {
-    last_ten.splice(-1, 1);
+    last_ten.splice(0, 1);
   }
   last_ten.push(now);
   tot = 0;
@@ -15,17 +16,45 @@ function last_ten_avg(now) {
     tot+=last_ten[i];
   }
 
-  return Math.round((tot/last_ten.length) * 100) / 100;
+  var last_ten_avg_new = Math.round((tot/last_ten.length) * 100) / 100;
+
+  last_ten_avg = (last_ten_avg+last_ten_avg_new) / 2;
+
+  return last_ten_avg;
+}
+
+function level(tick) {
+  var scartamento = (tick-last_ten_avg) / last_ten_avg;
+  console.log("scartamento: " + scartamento);
+
+  var color = "red";
+
+  switch (true) {
+    case (scartamento > 0.2):
+      color = "green";
+      break;
+    case (scartamento >= 0):
+      color = "blue";
+      break;
+    case (scartamento > -0.1):
+      color = "yellow";
+      break;
+    case (scartamento > -0.2):
+      color = "orange";
+      break;
+  }
+
+  return color;
 }
 
 var channel = pusher.subscribe('currency');
 channel.bind('last_trade', function(data) {
   console.log(data);
   if ( data.symbol === "BTCEUR") {
-    var title = data.value + "€ ~ " + last_ten_avg(data.value) + "€";
+    var title = data.value + "€ ~ " + do_last_ten_avg(data.value) + "€";
     var body = data.volume + "€ per " + data.quantity + "BTC";
 
-    var cmd = "notify-send -i ~/Documents/the-rock-trading-trades/pusher-node/trt32.png '" +  title + "' '" + body + "'";
+    var cmd = "notify-send -i ~/Documents/the-rock-trading-trades/pusher-node/trt32." + level(data.value) + ".png '" +  title + "' '" + body + "'";
     console.log(cmd);
 
     exec(cmd, function(error, stdout, stderr) {
