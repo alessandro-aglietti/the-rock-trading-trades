06a5e39c63f3db3f351dcb927b2deda031acc66d
Your Name <alessandro.aglietti@gmail.com>
2016-09-02 11:00:06 +0200

    fix avg math and dinamic icon path

--- pusher-node/index.js
+++ pusher-node/index.js
@@ -4,7 +4,7 @@ var exec = require('child_process').exec;
 var pusher = new Pusher("8458eb6fbd288f0cf3d8");
 
 var last_ten = [];
-var last_ten_avg = 0;
+var last_ten_avg = false;
 
 function do_last_ten_avg(now) {
   if ( last_ten.length > 10 ) {
@@ -18,8 +18,7 @@ function do_last_ten_avg(now) {
 
   var last_ten_avg_new = Math.round((tot/last_ten.length) * 100) / 100;
 
-  last_ten_avg = (last_ten_avg+last_ten_avg_new) / 2;
-
+  last_ten_avg = (!last_ten_avg) ? last_ten_avg_new : (last_ten_avg+last_ten_avg_new) / 2;
   return last_ten_avg;
 }
 
@@ -54,7 +53,7 @@ channel.bind('last_trade', function(data) {
     var title = data.value + "€ ~ " + do_last_ten_avg(data.value) + "€";
     var body = data.volume + "€ per " + data.quantity + "BTC";
 
-    var cmd = "notify-send -i ~/Documents/the-rock-trading-trades/pusher-node/trt32." + level(data.value) + ".png '" +  title + "' '" + body + "'";
+    var cmd = "notify-send -i " + __dirname + "/trt32." + level(data.value) + ".png '" +  title + "' '" + body + "'";
     console.log(cmd);
 
     exec(cmd, function(error, stdout, stderr) {
